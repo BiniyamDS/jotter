ab9e80994c00c42e01907d5c4b8cfcf5daca2d20
BiniyamDS <biniyam.yr@gmail.com>
2024-04-18 10:59:23 +0300

    fixed postcard ui issue

--- src/client/components/PostCard.jsx
+++ src/client/components/PostCard.jsx
@@ -12,10 +12,7 @@ const PostCard = ({ post }) => {
       className="w-1/2 p-2 hover:border-2 hover:cursor-pointer"
       onClick={handleClick}
     >
-      <img
-        className="object-fill w-full"
-        src={image}
-      />
+      <img className="w-full  max-h-64" src={image} />
       <h1 className="text-xl font-bold p-2">{title}</h1>
       <p className="px-2 text-gray-500">By {createdBy}</p>
     </li>
