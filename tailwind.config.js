256e4119abf0161be07622fe3fd27e0bfdd478d1
BiniyamDS <biniyam.yr@gmail.com>
2024-04-21 18:36:26 +0300

    improved font styling on paragraphs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'garmond': ['EB Garamond', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
